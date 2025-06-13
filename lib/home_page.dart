5ce78dc493c0bafdedccb68e346d71578f621209
SegnonDimas <segnondimas@gmail.com>
2025-06-13 10:44:42 +0100

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Accueil'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 80),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLJSxKWA93Qe7kr3-R6l3BJpQd-_et3qRQ5A&s',
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isRememberMe = true;
+
+  void login(BuildContext context, String email, String password) {
+    if (email.isEmpty || password.isEmpty) {
+      // Show error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Veuillez remplir tous les champs')),
+      );
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Connexion réussie')));
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+        /*(route) => false,*/
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        title: const Text('Login Page'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              //espace
+              const SizedBox(height: 80),
+              // icon de connexion
+              Icon(Icons.lock, size: 120, color: Colors.grey),
+              const SizedBox(height: 20), // espace
+              // champ de saisie de l'adresse email
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              const SizedBox(height: 20),
+
+              // champ de saisie du mot de passe
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+
+              // case à cocher "Se souvenir de moi"
+              Row(
+                children: [
+                  Radio(
+                    value: isRememberMe,
+                    groupValue: true,
+                    onChanged: (b) {
+                      setState(() {
+                        isRememberMe = !isRememberMe;
+                      });
+                    },
+                    toggleable: true,
+                    activeColor: Colors.deepPurple,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isRememberMe = !isRememberMe;
+                      });
+                    },
+                    child: Text('Se souvenir de moi'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 80),
+
+              // bouton de connexion
+              GestureDetector(
+                onTap: () {
+                  login(context, emailController.text, passwordController.text);
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.35,
+                    vertical: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
