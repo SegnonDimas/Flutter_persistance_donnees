9fce7478ec3a906c5b3c5054e2071cb255f73e82
SegnonDimas <segnondimas@gmail.com>
2025-06-20 12:41:09 +0100

    Ajout de numéro de téléphone pour la connexion. Persistance de images uploader sur l'appareil

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,37 +1,96 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_persistence_donnees/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Accueil'),
-        centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        foregroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 10),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLJSxKWA93Qe7kr3-R6l3BJpQd-_et3qRQ5A&s',
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  height: MediaQuery.of(context).size.height * 0.25,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(height: 20),
-            ],
-          ),
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Accueil'),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
         ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLJSxKWA93Qe7kr3-R6l3BJpQd-_et3qRQ5A&s',
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                CachedNetworkImage(
+                  imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8r1TvZrD8224vZqkDaRQ17v5vpU2BWfvBhTHBakKQAD_CBN0LYvGYVVUycbnENaukDWQ&usqp/=CAU12058",
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      CircularProgressIndicator(
+                        value: downloadProgress.progress,
+                      ),
+                  errorWidget: (context, url, error) => Icon(
+                    Icons.broken_image_sharp,
+                    size: 150,
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    final SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    // enregistrer le choix de l'utilisateur
+                    await prefs.setBool('rememberMe', false);
+
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                    /*login(
+                              context,
+                              emailController.text,
+                              passwordController.text,
+                            );*/
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.2,
+                      vertical: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: BoxBorder.all(color: Colors.red.shade700),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      'Se déconnecter',
+                      style: TextStyle(
+                        color: Colors.red.shade700,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ), // bouton de connexion
       ),
     );
   }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  final String? email;
+  final String? password;
+  const LoginPage({super.key, this.email, this.password});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -13,24 +16,55 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
 
   bool isRememberMe = true;
+  rechargeInformations() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    // récupérer le choix de l'utilisateur
+    isRememberMe = prefs.getBool('rememberMe') ?? true;
+    emailController.text = prefs.getString('email') ?? '';
+    passwordController.text = prefs.getString('password') ?? '';
+    phoneController.text = prefs.getString('phone') ?? '';
+  }
+
+  //suppression des informations de connexion
+  Future<void> clearLoginData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('email');
+    await prefs.remove('password');
+    await prefs.remove('phone');
+    await prefs.remove('rememberMe');
+  }
 
   Future<void> login(
     BuildContext context,
     String email,
     String password,
+    String phone,
   ) async {
-    if (email.isEmpty || password.isEmpty) {
+    if (email.isEmpty && phone.isEmpty) {
       // Show error message
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Veuillez remplir tous les champs')),
+        const SnackBar(
+          content: Text(
+            'Veuillez remplir au moins votre adresse gmail ou votre numéro de téléphone',
+          ),
+        ),
+      );
+    } else if (password.isEmpty) {
+      // Show error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Veuillez saisir votre mot de passe')),
       );
     } else {
       // Obtain shared preferences.
       final SharedPreferences prefs = await SharedPreferences.getInstance();
       // enregistrer le choix de l'utilisateur
       await prefs.setBool('rememberMe', isRememberMe);
+      await prefs.setString('email', email);
+      await prefs.setString('password', password);
+      await prefs.setString("phone", phone);
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(const SnackBar(content: Text('Connexion réussie')));
@@ -44,10 +78,14 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
+  void initState() {
+    rechargeInformations();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
-      top: false,
-      bottom: false,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.deepPurple,
@@ -61,9 +99,24 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children: <Widget>[
                 //espace
-                const SizedBox(height: 80),
+                const SizedBox(height: 30),
+
                 // icon de connexion
-                Icon(Icons.lock, size: 120, color: Colors.grey),
+                CachedNetworkImage(
+                  imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8r1TvZrD8224vZqkDaRQ17v5vpU2BWfvBhTHBakKQAD_CBN0LYvGYVVUycbnENaukDWQ&usqp/=CAU12058",
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      CircularProgressIndicator(
+                        value: downloadProgress.progress,
+                      ),
+                  errorWidget: (context, url, error) => Icon(
+                    Icons.broken_image_sharp,
+                    size: 150,
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+
+                //Icon(Icons.lock, size: 120, color: Colors.grey),
                 const SizedBox(height: 20), // espace
                 // champ de saisie de l'adresse email
                 TextFormField(
@@ -71,6 +124,13 @@ class _LoginPageState extends State<LoginPage> {
                   decoration: const InputDecoration(labelText: 'Email'),
                 ),
                 const SizedBox(height: 20),
+
+                // champ de saisie du téléphone
+                TextFormField(
+                  controller: phoneController,
+                  decoration: const InputDecoration(labelText: 'Phone'),
+                  keyboardType: TextInputType.phone,
+                ),
 
                 // champ de saisie du mot de passe
                 TextFormField(
@@ -113,6 +173,7 @@ class _LoginPageState extends State<LoginPage> {
                       context,
                       emailController.text,
                       passwordController.text,
+                      phoneController.text,
                     );
                   },
                   child: Container(
@@ -128,6 +189,18 @@ class _LoginPageState extends State<LoginPage> {
                       'Se connecter',
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
+                  ),
+                ),
+
+                // bouton de connexion
+                GestureDetector(
+                  onTap: () async {
+                    await clearLoginData();
+                    ;
+                  },
+                  child: const Text(
+                    'Supprimer les informations de connexion',
+                    style: TextStyle(color: Colors.deepPurple, fontSize: 16),
                   ),
                 ),
               ],
